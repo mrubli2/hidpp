db9ce4457c17f29b1e6640d30b3f8fdab7766f78
Martin Rubli <martin_rubli@logitech.com>
2022-09-22 21:13:24 +0200

    libhidpp: hidpp10: Fix build warnings
    In file included from ../subprojects/hidpp/src/libhidpp/hidpp10/DeviceInfo.h:24,
                     from ../subprojects/hidpp/src/libhidpp/hidpp/DeviceInfo.cpp:22:
    ../subprojects/hidpp/src/libhidpp/hidpp10/Sensor.h:27:7: warning: ‘class HIDPP10::Sensor’ has virtual functions and accessible non-virtual destructor [-Wnon-virtual-dtor]
       27 | class Sensor
          |       ^~~~~~
    ../subprojects/hidpp/src/libhidpp/hidpp10/Sensor.h:37:7: warning: base class ‘class HIDPP10::Sensor’ has accessible non-virtual destructor [-Wnon-virtual-dtor]
       37 | class ListSensor: public Sensor
          |       ^~~~~~~~~~
    ../subprojects/hidpp/src/libhidpp/hidpp10/Sensor.h:37:7: warning: ‘class HIDPP10::ListSensor’ has virtual functions and accessible non-virtual destructor [-Wnon-virtual-dtor]
    ../subprojects/hidpp/src/libhidpp/hidpp10/Sensor.h:63:7: warning: base class ‘class HIDPP10::Sensor’ has accessible non-virtual destructor [-Wnon-virtual-dtor]
       63 | class RangeSensor: public Sensor
          |       ^~~~~~~~~~~
    ../subprojects/hidpp/src/libhidpp/hidpp10/Sensor.h:63:7: warning: ‘class HIDPP10::RangeSensor’ has virtual functions and accessible non-virtual destructor [-Wnon-virtual-dtor]

--- src/libhidpp/hidpp10/Sensor.h
+++ src/libhidpp/hidpp10/Sensor.h
@@ -27,6 +27,8 @@ namespace HIDPP10
 class Sensor
 {
 public:
+	virtual ~Sensor() {}
+
 	virtual unsigned int fromDPI (unsigned int dpi) const = 0;
 	virtual unsigned int toDPI (unsigned int internal_value) const = 0;
 
